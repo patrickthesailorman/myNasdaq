2a50b38c0a666e2a287ed43c15554ed15ee7ea2c
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-21 00:57:50 +0000

    Add index.html

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', process.env.PORT);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    console.log("GET the homepage");
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', function(req, res){
+    console.log("GET the json");
+    res
+    .status(200)
+    .send({ "jsonData" : true });
+});
+
+app.get('/file', function(req, res){
+    console.log("GET the file");
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
+});
 
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
