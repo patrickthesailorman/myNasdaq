f83a7dc96140cd6a520efcef3cb8900a14bed5c6
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-21 00:02:29 +0000

    add Port

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("It Works!!!");
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT);
+
+
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+console.log('Magic happens on port: ' + port);
+ });
