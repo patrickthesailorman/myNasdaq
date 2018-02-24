ff0aaa5731c877b0c959ce9ef5f17a8fd9d62746
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-24 17:22:52 +0000

    Add offset & count

--- api/controllers/companies.controllers.js
+++ api/controllers/companies.controllers.js
@@ -2,9 +2,24 @@ var companyData = require('../data/company-list.json');
 
 module.exports.companiesGetAll = function(req, res) {
     console.log("GET the companies");
+    console.log(req.query);
+    
+    var offset = 0;
+    var count = 5;
+    
+    if (req.query && req.query.offset) {
+        offset = parseInt(req.query.offset, 10);
+    }
+    
+     if (req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
+    }
+    
+    var returnData = companyData.slice(offset, offset + count);
+    
     res
     .status(200)
-    .send(companyData);
+    .send(returnData);
 };
 
 module.exports.companiesGetOne= function(req, res) {
