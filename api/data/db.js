5c2ef28b7de88cc5eefe2305ca022f333e8627f7
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-25 23:39:06 +0000

    Add gracefulShutdown

--- api/data/db.js
+++ api/data/db.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 var dburl = 'mongodb://' + process.env.IP + ':27017/myNasdaq';
+var retry = null;
 
 mongoose.connect(dburl);
 
+// CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
     console.log('Mongoose connected to ' + dburl);
 });
@@ -15,23 +17,32 @@ mongoose.connection.on('error', function(err) {
     console.log('Monggose connection error: ' + err);
 });
 
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+function gracefulShutdown(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+}
+
+// For APP termination
 process.on('SIGINT', function() {
-    mongoose.connection.close(function() {
-        console.log('Mongoose disconnected through app termination (SIGINT)');
+    gracefulShutdown('App termination (SIGINT)', function() {
         process.exit(0);
     });
 });
 
+// For Heroku APP termination
 process.on('SIGTERM', function() {
-    mongoose.connection.close(function() {
-        console.log('Mongoose disconnected through app termination (SIGTERM)');
+    gracefulShutdown('App termination (SIGTERM)', function() {
         process.exit(0);
     });
 });
 
+// For nodemon restarts
 process.once('SIGUSR2', function() {
-    mongoose.connection.close(function() {
-        console.log('Mongoose disconnected through app termination (SIGUSR2)');
+    gracefulShutdown('nodemon restart', function() {
         process.kill(process.pid, 'SIGUSR2');
     });
 });
