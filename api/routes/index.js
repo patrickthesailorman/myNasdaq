7c1663f390b8c6071fd0e5808ee08a6c1e2f7f07
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-27 01:19:04 +0000

    Add routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,18 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 var ctrlCompanies = require('../controllers/companies.controllers.js'); 
+var ctrlUsers = require('../controllers/users.controllers.js');
 
 // Company Routes
 router
-    .route('/companies')
-    .get(ctrlCompanies.companiesGetAll)
-    .post(ctrlCompanies.companiesAddOne);
+  .route('/companies')
+  .get(ctrlCompanies.companiesGetAll)
+  .post(ctrlCompanies.companiesAddOne);
  
- router
-    .route('/companies/:companyId')
-    .get(ctrlCompanies.companiesGetOne)
-    .put(ctrlCompanies.companiesUpdateOne)
-    .delete(ctrlCompanies.companiesDeleteOne);
+router
+  .route('/companies/:companyId')
+  .get(ctrlCompanies.companiesGetOne)
+  .put(ctrlCompanies.companiesUpdateOne)
+  .delete(ctrlCompanies.companiesDeleteOne);
 
+// Authentication
+router
+  .route('/users/register')
+  .post(ctrlUsers.register);
+
+  router
+    .route('/users/login')
+    .post(ctrlUsers.login);
     
 module.exports = router;
