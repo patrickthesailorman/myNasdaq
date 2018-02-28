509ef870b550ab29001a9f75e66fb2a0539b52c8
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-28 21:47:21 +0000

    Fix title

--- public/angular-app/company-list/company-list-controller.js
+++ public/angular-app/company-list/company-list-controller.js
@@ -2,7 +2,7 @@ angular.module('myNasdaq').controller('CompaniesController', CompaniesController
 
 function CompaniesController(companyDataFactory) {
   var vm = this;
-  vm.title = 'myNASDAQ App';
+  vm.title = 'myNASDAQ';
   companyDataFactory.companyList().then(function(response) {
     // console.log(response);
     vm.companies = response.data;
