b961e469b0653406a8287415e9b32881bd36ca57
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-02 02:26:03 +0000

    Add Pagination

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -1,4 +1,4 @@
-angular.module('myNasdaq', ['ngRoute', 'angular-jwt']).config(config).run(run);
+angular.module('myNasdaq', ['ngRoute', 'angular-jwt','angularUtils.directives.dirPagination']).config(config).run(run);
 
 function config($httpProvider, $routeProvider) {
     $httpProvider.interceptors.push('AuthInterceptor');
